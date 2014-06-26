71246d954903a07ac211998dfb3dfed6947eef2c
Niclas <mietz.niclas@gmail.com>
2014-06-26 16:08:49 +0200

    Refactor KontaktDAO

--- src/main/java/persistence/KontaktDAO.java
+++ src/main/java/persistence/KontaktDAO.java
@@ -3,7 +3,9 @@ package persistence;
 import java.io.IOException;
 
 import model.Kontakt;
-
+/**
+ * Abstractor for the DAO
+ */
 public interface KontaktDAO {
 	public void Save(Kontakt contact) throws Exception;
 }
